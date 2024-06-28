61833f237d72f0ce5c61b7d1df2a8faf407c05f2
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:42:56 -0700

    Q13 Answer

---
+++ 13.Your Own Array/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ["Bike", "Racing Car", "Aeroplane", "Helicopter", "Ship"];
+for (let index = 0; index < transportation.length; index++) {
+    console.log("I Would Like To Have My Own " + transportation[index]);
+}
---
+++ 13.Your Own Array/app.ts
@@ -0,0 +1,4 @@
+let transportation:string[]=["Bike","Racing Car","Aeroplane","Helicopter","Ship"];
+for (let index = 0; index < transportation.length; index++) {
+     console.log("I Would Like To Have My Own " + transportation[index]);
+}
