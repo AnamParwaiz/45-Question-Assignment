aacff400fd46358ff1c938ccc28446c99950c943
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:39:22 -0700

    Q9 Answer

---
+++ 9.Favourite Number/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let favNumber = 1;
+let favNumber2 = 2;
+console.log(`My Favourite Number Is "${favNumber}" And "${favNumber2}`);
---
+++ 9.Favourite Number/app.ts
@@ -0,0 +1,3 @@
+let favNumber:number=1;
+let favNumber2:number=2; 
+console.log(`My Favourite Number Is "${favNumber}" And "${favNumber2}`);
