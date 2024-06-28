33c208d883978dd569b6de901d59f6cf64c92ba6
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:42:09 -0700

    Q12 Answer

---
+++ 12.Greetings/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ["Umaima", "Mustajab", "Ammara", "Zahra", "Hiba", "Samra"];
+let message1 = ' Assalam O Alaikum ! ';
+let message2 = ' How Are You And How Was Your Day? ';
+for (let index = 0; index < names.length; index++) {
+    console.log(message1 + names[index] + message2);
+}
---
+++ 12.Greetings/app.ts
@@ -0,0 +1,6 @@
+let names:string[]=["Umaima","Mustajab","Ammara","Zahra","Hiba","Samra"];
+let message1:string=' Assalam O Alaikum ! ';
+let message2:string=' How Are You And How Was Your Day? ';
+for (let index = 0; index < names.length; index++){
+    console.log(message1  +  names[index]  +  message2);
+} 
