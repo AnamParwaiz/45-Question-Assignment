963b34f7fea89d30d00c545a215f90c15419aef1
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:36:25 -0700

    Q4 Answer

---
+++ 4.Famous Quote/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let famousQuote = "Everybody should learn to program a computer because it teaches you how to think";
+let author = "Steve Jobs";
+console.log(`${author} once said , "${famousQuote}"`);
---
+++ 4.Famous Quote/app.ts
@@ -0,0 +1,4 @@
+let famousQuote: string = "Everybody should learn to program a computer because it teaches you how to think";
+let author:string="Steve Jobs";
+
+console.log(`${author} once said , "${famousQuote}"`);
