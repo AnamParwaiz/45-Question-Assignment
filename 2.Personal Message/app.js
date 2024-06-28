382191812cd02015a3ba3a66a3857bccb8be6331
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:34:06 -0700

    Q2

---
+++ 2.Personal Message/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let personname = "Anam Parwaiz";
+let message = "would you like to learn some Typescript today?";
+console.log(`"Hello ${personname} , ${message}"`);
---
+++ 2.Personal Message/app.ts
@@ -0,0 +1,4 @@
+let personName:string="Anam Parwaiz";
+let message:string="would you like to learn some Typescript today?";
+console.log(`"Hello ${personName} , ${message}"`);
+
