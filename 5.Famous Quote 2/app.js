434528cf04f2a9248ce72772efcd9ba06b5360e9
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:37:33 -0700

    Q5 Answer

---
+++ 5.Famous Quote 2/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let famousquote = "In order to be irreplaceable, one must always be different.";
+let famous_person = "Coco Chanel";
+let message = `${famous_person} once said , "${famousquote}"`;
+console.log(message);
---
+++ 5.Famous Quote 2/app.ts
@@ -0,0 +1,4 @@
+let famousquote="In order to be irreplaceable, one must always be different.";
+let famous_person="Coco Chanel";
+let message=`${famous_person} once said , "${famousquote}"`;
+console.log(message);
