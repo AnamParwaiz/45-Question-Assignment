26de23c4665cc8ec97da8436baa1dce927c3bf26
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:38:12 -0700

    Q6 Answer

---
+++ 6.Stripping Names/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let personName = `\n\t ANAM \n\t\n\t PARWAIZ \t\n`;
+console.log(personName);
+let stripped = personName.trim();
+console.log(stripped);
---
+++ 6.Stripping Names/app.ts
@@ -0,0 +1,6 @@
+let personName:string=`\n\t ANAM \n\t\n\t PARWAIZ \t\n`;
+console.log(personName);
+
+let stripped:string=personName.trim();
+console.log(stripped);
+
