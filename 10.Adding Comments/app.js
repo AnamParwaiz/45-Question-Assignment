77b05433e9ae309594721987092fe758e8355b29
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:40:07 -0700

    Q10 Answer

---
+++ 10.Adding Comments/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Exercise 10 //
+// Name:Anam Parwaiz //
+// Dated:3-5-24 //
+// this is a program which will run and showed which have i already wrote //
+console.log("Hello Everyone ! I am a student of Governer House Of Sindh");
---
+++ 10.Adding Comments/app2.js
@@ -0,0 +1,7 @@
+"use strict";
+// Exercise 10 //
+// Name:Anam Parwaiz //
+// Dated:3-5-24 //
+// this is a program of mathematical operation which will run and solved to showed//
+// 25 exponential 4//
+console.log(25 ** 4);
