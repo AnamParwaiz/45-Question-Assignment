e6af0ffe195d358d112dbadd7d81ae2536b306e4
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:47:09 -0700

    Q25 , Q26 And Q27 Answers

---
+++ 25 , 26 , 27.Alien Colors/app.js
@@ -0,0 +1,61 @@
+// Exercise # 26 (Alien Colors # 1)
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("The Player Just Earned 5 Points.");
+}
+;
+if (alien_color == "yellow") {
+    console.log("The Player Just Earned 5 Points");
+}
+;
+// Exercise # 26 (Alien Colors # 2)
+if (alien_color == "green") {
+    console.log("The Player Just Earned 5 Points For Shooting The Alien.");
+}
+else {
+    console.log("The Player Just Earned 10 Points");
+}
+if (alien_color == "red") {
+    console.log("The Player Just Earned 5 Points For Shooting The Alien.");
+}
+;
+// Exercise # 27 (Alien Colors # 3)
+// VERSION # 1 //
+var alien_color1 = "green";
+if (alien_color1 == "green") {
+    console.log("The Player Just Earned 5 Points");
+}
+else if (alien_color1 == "yellow") {
+    console.log("The Player Just Earned 10 Points");
+}
+else if (alien_color1 == "red") {
+    console.log("The Player Just Earned 15 Points");
+}
+else
+    (console.log("Please Select The Right Color !"));
+// VERSION # 2 //
+var alien_color2 = "yellow";
+if (alien_color2 == "green") {
+    console.log("The Player Just Earned 5 Points");
+}
+else if (alien_color2 == "yellow") {
+    console.log("The Player Just Earned 10 Points");
+}
+else if (alien_color2 == "red") {
+    console.log("The Player Just Earned 15 Points");
+}
+else
+    (console.log("Please Select The Right Color !"));
+// VERSION # 3 //
+var alien_color3 = "red";
+if (alien_color3 == "green") {
+    console.log("The Player Just Earned 5 Points");
+}
+else if (alien_color3 == "yellow") {
+    console.log("The Player Just Earned 10 Points");
+}
+else if (alien_color3 == "red") {
+    console.log("The Player Just Earned 15 Points");
+}
+else
+    (console.log("Please Select The Right Color !"));
