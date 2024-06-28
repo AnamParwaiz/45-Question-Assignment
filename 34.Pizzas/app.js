ae934a38c5c3cec936be84da86e15b5c8a10c927
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:49:53 -0700

    Q34 Answer

---
+++ 34.Pizzas/app.js
@@ -0,0 +1,5 @@
+var fav_pizzas = ["Pepperoni Pizza", "Mexican Pizza", "Cheese Pizza"];
+for (var index = 0; index < fav_pizzas.length; index++) {
+    console.log("I Like To Eat ".concat(fav_pizzas[index], " ! "));
+}
+console.log("\nI Really Like Pizzas ! Pizza Is An Italian Food That Was Created In Italy (The Naples Area). It Is Made With Different Toppings And Individual Loves . Some Of The Most Common Toppings Are Cheese, Sausages, Pepperoni, Vegetables, Tomatoes, Spices And Herbs And Basil.So I Really Love Pizzas Sooo Much !");
---
+++ 34.Pizzas/app.ts
@@ -0,0 +1,7 @@
+let fav_pizzas:string[]=["Pepperoni Pizza","Mexican Pizza","Cheese Pizza"];
+
+for (let index = 0; index < fav_pizzas.length; index++) {
+    console.log(`I Like To Eat ${fav_pizzas[index]} ! `)
+}
+
+console.log("\nI Really Like Pizzas ! Pizza Is An Italian Food That Was Created In Italy (The Naples Area). It Is Made With Different Toppings And Individual Loves . Some Of The Most Common Toppings Are Cheese, Sausages, Pepperoni, Vegetables, Tomatoes, Spices And Herbs And Basil.So I Really Love Pizzas Sooo Much !");
