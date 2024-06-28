be417b9057af5a80f2fce375bb4be4abb842dc6d
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:38:47 -0700

    Q7 And Q8 Answer

---
+++ 7 , 8. Number 8/app.js
@@ -0,0 +1,17 @@
+"use strict";
+function add(digit, digTwo) {
+    console.log(digit + digTwo);
+}
+add(2, 6);
+function subtract(digit, digTwo) {
+    console.log(digit - digTwo);
+}
+subtract(12, 4);
+function multiply(digit, digTwo) {
+    console.log(digit * digTwo);
+}
+multiply(2, 4);
+function divide(digit, digTwo) {
+    console.log(digit / digTwo);
+}
+divide(24, 3);
---
+++ 7 , 8. Number 8/app.ts
@@ -0,0 +1,19 @@
+function add(digit:number , digTwo:number){
+    console.log(digit + digTwo);
+}
+add (2 , 6);
+
+function subtract(digit:number , digTwo:number){
+     console.log(digit - digTwo);
+}
+subtract(12 , 4);
+
+function multiply(digit:number , digTwo:number){
+    console.log(digit * digTwo);
+}
+multiply(2 , 4);
+
+function divide(digit:number , digTwo:number){
+    console.log(digit / digTwo);
+}
+divide(24 , 3);
