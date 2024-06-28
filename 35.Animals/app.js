ee2fe08571ba9e13a95fec829064843b80802574
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:50:13 -0700

    Q35 Answer

---
+++ 35.Animals/app.js
@@ -0,0 +1,8 @@
+var animals = ["Cow", "Horse", "Goat"];
+// for (var index = 0; index < animals.length; index++) {
+//     console.log(" A ".concat(animals[index], " Would Make A Great Pet."));
+// }
+animals.forEach(function (animals) {
+    console.log("A ".concat(animals, " Would Make A Great Pet."));
+});
+console.log("Any of these animals would make a great pet!");
---
+++ 35.Animals/app.ts
@@ -0,0 +1,11 @@
+let animals:string[]=["Cow","Horse","Goat"];
+
+// for (var index = 0; index < animals.length; index++) {
+//     console.log(" A ".concat(animals[index], " Would Make A Great Pet."));
+// }
+
+ animals.forEach(animals =>{
+     console.log(`A ${animals} Would Make A Great Pet.`);
+ })
+
+console.log("Any of these animals would make a great pet!");
