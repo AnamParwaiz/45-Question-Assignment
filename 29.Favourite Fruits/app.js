5fd259918b1e021ff7d1634efe01bc5259e11e78
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:48:02 -0700

    Q29 Answer

---
+++ 29.Favourite Fruits/app.js
@@ -0,0 +1,16 @@
+var favourite_fruits = ["Apple", "Pomegranate", "Banana"];
+if (favourite_fruits.includes("Apple")) {
+    console.log("I Really Like Apple");
+}
+if (favourite_fruits.includes("Guava")) {
+    console.log("I Really Like Guava");
+}
+if (favourite_fruits.includes("Banana")) {
+    console.log("I Really Like Banana");
+}
+if (favourite_fruits.includes("Mango")) {
+    console.log("I Really Like Mango");
+}
+if (favourite_fruits.includes("Pomegranate")) {
+    console.log("I Really Like Pomegranate");
+}
