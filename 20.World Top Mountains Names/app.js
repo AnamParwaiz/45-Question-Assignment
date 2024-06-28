c15ec6d60c9e83e8c3f42a915ead2b06d74f83f6
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:45:02 -0700

    Q20 Answer

---
+++ 20.World Top Mountains Names/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let mountains = ["1.Mount Everest", "2.Mount Godwin Austen", "3.Kangchenjunga", "4.Lhotse", "5.Makalu", "6.Cho Oyu", "7.Dhaulagiri", "8.Manaslu", "9.Nanga Parbat", "10.Annapurna"];
+console.log('WORLD TOP 10 MOUNTAINS ARE : ');
+for (let TOP of mountains) {
+    console.log(TOP);
+}
