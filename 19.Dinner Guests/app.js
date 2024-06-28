ff5f32b28e94dc382d6eb5b1b1773230da3e6c67
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:44:35 -0700

    Q19 answer

---
+++ 19.Dinner Guests/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(`\nNumber Of People I Inviting On Dinner Party At 5 Star Hotel\n`);
+const app_1 = require("../14 , 15 , 16 , 17.Guest List/app");
+console.log(`${app_1.guest_list}`);
---
+++ 19.Dinner Guests/app.ts
@@ -0,0 +1,4 @@
+console.log(`\nNumber Of People I Inviting On Dinner Party At 5 Star Hotel\n`);
+import{ guest_list } from '../14 , 15 , 16 , 17.Guest List/app';
+console.log(`${guest_list}`);
+
