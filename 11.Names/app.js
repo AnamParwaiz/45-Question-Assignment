3b7b5b47d27cd454d998a41f47a43a494ceb47ff
AnamParwaiz <160168812+AnamParwaiz@users.noreply.github.com>
2024-06-28 09:40:48 -0700

    Q11 Answer

---
+++ 11.Names/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let sentence = "Some Of my Friends Name Are :";
+console.log(sentence);
+let names = ["Umaima", "Mustajab", "Ammara", "Zahra", "Hiba", "Samra"];
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index]);
+}
---
+++ 11.Names/app.ts
@@ -0,0 +1,8 @@
+let sentence="Some Of my Friends Name Are :"
+console.log(sentence);
+
+let names:string[]=["Umaima","Mustajab","Ammara","Zahra","Hiba","Samra"];
+for (let index = 0; index < names.length; index++){
+    console.log(names[index]);
+} 
+    
